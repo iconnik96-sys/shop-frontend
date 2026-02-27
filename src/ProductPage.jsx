011c0d0ee35afa2a68f38313b66eb45b8130ac5b
Nikhil-Ingale <iconnik96@gmail.com>
2026-02-27 10:28:55 +0530

    fdsaf

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -11,7 +11,7 @@ const ProductPage = () => {
 
   const [editId, setEditId] = useState(null);
 
-  const BASE_URL = "https://shop-backend-b8e2.onrender.com/product/getallproducts";
+  const BASE_URL = "https://shop-backend-b8e2.onrender.com/product";
 
   // 🔹 Fetch All Products
   const fetchProducts = async () => {
